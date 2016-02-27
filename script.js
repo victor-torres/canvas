0687317f3d5a46dc9e89f2dcc8e0627a8ccc7484
Victor Torres <vpaivatorres@gmail.com>
2016-02-27 18:50:36 -0300

    Adding AngularJS selectize and Facebook model canvas.

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-var app = angular.module('canvasApp', []);
+var app = angular.module('canvasApp', ['selectize']);
 
 app.controller('CanvasController', function($scope, $http) {
-  $http.get('json/example.json').then(
-    function(res){
-      $scope.canvas = res.data;
+  $http.get('json/index.json').then(
+    function(res) {
+      $scope.canvasIndex = res.data.index;
+      $scope.selectedCanvas = $scope.canvasIndex[0];
+      $scope.selectizeOptions = $scope.canvasIndex;
     });
+
+  $scope.selectizeConfig = {
+    create: false,
+    valueField: 'file',
+    labelField: 'name',
+    searchField: ['file', 'name', 'description'],
+    delimiter: '|',
+    placeholder: 'Choose a canvas',
+    onInitialize: function(selectize) {
+      // receives the selectize object as an argument
+    },
+    render: {
+      item: function(item, escape) {
+        return '<div>' +
+          (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
+          (item.description ? '<span class="description">' + escape(item.description) + '</span>' : '') +
+          '</div>';
+      },
+      option: function(item, escape) {
+        var label = item.name || item.description;
+        var caption = item.name ? item.description : null;
+        return '<div>' +
+          '<span class="label">' + escape(label) + '</span>' +
+          (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
+          '</div>';
+      }
+    },
+    maxItems: 1
+  };
+
+  $scope.$watch("selectedCanvas", function(filename) {
+    if(!filename || typeof(filename) != 'string') {
+      return;
+    }
+    console.log('loading', filename);
+    $http.get('json/' + filename).then(
+      function(res) {
+        console.log(res.data);
+        $scope.canvas = res.data;
+      });
+  }, true);
 });
 
 app.directive('card', function() {
